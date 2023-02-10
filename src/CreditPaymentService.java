9d66127664dbf4f057838ebc7b1bdd6b96743d72
QA <feverx@inbox.ru>
2023-02-10 23:07:01 +0300

    final commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate(int summ, int time, double percentYear) {
+        double monthPay;
+        double partMonthPercent = (percentYear / 12) / 100;
+        monthPay = summ * (partMonthPercent + partMonthPercent / (Math.pow((1 + partMonthPercent), time) - 1));
+        return monthPay;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int summ = 1_000_000;
+        int time = 12;
+        double percentYear = 9.99;
+        double monthPay = service. calculate(summ, time, percentYear);
+        System.out.println("Ежемесячный платеж по кредиту: " + (int) monthPay);
+    }
+
+
 }
